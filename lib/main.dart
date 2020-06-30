b36d1033fbb02409043bbe726de9fcb6c377f3cf
mszire <mrunalzire@gmail.com>
2020-06-30 17:37:46 +0530

    my first commit
    added the flutter initial app

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_tutorials/widgets/nav-drawer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+  }
+}
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        drawer: NavDrawer(),
+        appBar: AppBar(title: Text('My First App')),
+        backgroundColor: Colors.white,
+        body: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: <Widget>[
+            Card(
+              color: Colors.redAccent,
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.redAccent,
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // AspectRatio(
+                  //   aspectRatio: 18.0 / 11.0,
+                  //   child: Image.asset('assets/diamond.png'),
+                  // ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.redAccent,
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // AspectRatio(
+                  //   aspectRatio: 18.0 / 11.0,
+                  //   child: Image.asset('assets/diamond.png'),
+                  // ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.redAccent,
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // AspectRatio(
+                  //   aspectRatio: 18.0 / 11.0,
+                  //   child: Image.asset('assets/diamond.png'),
+                  // ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
